9aca122cdfb1b9c4da396f594ca49977f0d1d3a7
Breyanna Vass <breyannavass@gmail.com>
2019-11-12 16:45:39 -0500

    setup the classes file

---
+++ classes.py
@@ -0,0 +1,13 @@
+# Create class
+class User:
+    # constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age}'
+
+    def has_birthday(self):
+        self.age += 1
