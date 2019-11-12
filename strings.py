dc716a25381afb9549d2a859dc9fd263a9054dff
Breyanna Vass <breyannavass@gmail.com>
2019-11-12 16:07:43 -0500

    understanding strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+# String in python are surround by either single or double quotation marks. Let's look at string formatting and some string methods
+
+name = 'Brad'
+age = 37
+
+# Concatenate
+print('Hello, my name is ' + name + ' and I am ' + str(age))
+
+# String Formatting
+
+# String Methods
