4264810e53a484ec6e0b21e443d12827c7ea825a
Breyanna Vass <breyannavass@gmail.com>
2019-11-12 16:00:46 -0500

    finished the variable file

--- variables.py
+++ variables.py
@@ -20,3 +20,13 @@ Variable Rules:
 
 # Multiple assignment
 x, y, name, is_cool = (1, 2.5, 'John', True)
+
+# Basic math
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(type(z), z)
