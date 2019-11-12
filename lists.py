1fd496c6ccfd222528d0b7aed6221cc8a7f6643c
Breyanna Vass <breyannavass@gmail.com>
2019-11-12 16:35:29 -0500

    understanding the lists

---
+++ lists.py
@@ -0,0 +1,19 @@
+# create list
+numbers = [1, 2, 3, 4, 5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# constructor
+# numbers2 = list((1, 2, 3, 4, 5))
+# print(numbers, numbers2)
+
+print(fruits[1])  # get a value
+
+print(len(fruits))  # length
+
+fruits.append('Mangos')  # append the list
+
+fruits.insert(2, 'Strawberries')  # insert into position
+
+fruits[0] = 'Blueberries'  # change value
+
+fruits.pop(2)  # remove the pop
