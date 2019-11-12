ba79b6d758d1a6205af15deb5b4d467669c704a2
Breyanna Vass <breyannavass@gmail.com>
2019-11-12 16:50:47 -0500

    extended classes

--- classes.py
+++ classes.py
@@ -11,3 +11,20 @@ class User:
 
     def has_birthday(self):
         self.age += 1
+
+# extended class
+
+
+class Customer(User):
+    # constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} and my balance is {self.balance}'
