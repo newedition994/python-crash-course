6c3de867bb2a477d1007abe12779c6efa42dcd76
Breyanna Vass <breyannavass@gmail.com>
2019-11-12 16:28:21 -0500

    understanding functions

---
+++ functions.py
@@ -0,0 +1,22 @@
+# function is a block of code which runs when it is called
+
+# Create function
+
+
+def sayHello(name='Sam'):
+    print(f'Hello {name}')
+
+# Return values
+
+
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+# lambda function is a small anonymous function; can take any number of arguments, but can only have one expression
+
+
+# getSum(num1, num2): return num1 + num2
+
+
+print(getSum(10, 3))
